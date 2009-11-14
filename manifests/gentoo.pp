44881d4c2497dbe42942e66c81632a2e4f60148a
mh <mh@immerda.ch>
2009-11-14 15:19:27 +0100

    update to 0.25.x style

---
+++ manifests/gentoo.pp
@@ -0,0 +1,5 @@
+class nginx::gentoo inherits nginx::base {
+    Package[nginx]{
+        category => 'www-servers',
+    }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,42 +2,9 @@
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "nginx": }
-
 class nginx {
     case $operatingsystem {
         gentoo: { include nginx::gentoo }
         default: { include nginx::base }
-    }
-}
-
-class nginx::base {
-    package{'nginx':
-        ensure => installed,
-    }
-
-    service{'nginx':
-        ensure => running,
-        enable => true,
-        hasstatus => false, 
-        require => Package[nginx],
-    }
-
-    file{nginx_config:
-        path => '/etc/nginx/nginx.conf',
-        source => [ "puppet://$server/files/nginx/${fqdn}/nginx.conf",
-                    "puppet://$server/files/nginx/nginx.conf",
-                    "puppet://$server/modules/nginx/nginx.conf" ],
-        owner => root,
-        group => 0,
-        mode => 0644,
-        require => Package[nginx],
-        notify => Service[nginx],
-    }
-}
-
-class nginx::gentoo inherits nginx::base {
-    Package[nginx]{
-        category => 'www-servers',
     }
 }
