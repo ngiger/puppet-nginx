d16266303ce9941ff0e1aaef7bfe0a5a453ad8c8
niklaus.giger@swissonline.ch <niklaus.giger@swissonline.ch>
2011-06-17 17:23:02 +0200

    Fixed working locally

--- manifests/base.pp
+++ manifests/base.pp
@@ -10,11 +10,14 @@ class nginx::base {
     require => Package[nginx],
   }
 
-  file{nginx_config:
+  file{"nginx_config":
     path => '/etc/nginx/nginx.conf',
     source => [ "puppet:///modules/site-nginx/${fqdn}/nginx.conf",
           "puppet:///modules/site-nginx/nginx.conf",
-          "puppet:///modules/nginx/nginx.conf" ],
+          "puppet:///modules/nginx/nginx.conf",
+	  # Without puppet to enable access to the modules
+          "modules/nginx/nginx.conf",
+	],
     owner => root,
     group => 0,
     mode => 0644,
